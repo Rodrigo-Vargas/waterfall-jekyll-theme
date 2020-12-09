1d0308c7e70dc8120de3191b598326837f3524aa
Rodrigo-Vargas <rodrigovargas123@gmail.com>
2020-12-09 20:32:04 -0300

    Improvements on mobile nav menu

--- front/js/nav-collapse.js
+++ front/js/nav-collapse.js
@@ -1,5 +1,7 @@
 class NavCollapse {
    constructor() {
+      this.panels = [];
+
       this.init();
    }
 
@@ -8,9 +10,29 @@ class NavCollapse {
 
       navTogglers.forEach(toggler => {
          toggler.addEventListener('click', event => {
+            event.preventDefault();
+            event.stopPropagation();
             document.querySelector('body').classList.toggle('nav-opened');
          });
+
+         this.panels.push(document.querySelector(toggler.getAttribute('href')));
       });
+
+      window.onclick = (event) => {
+         if (!this.elementIsAPanel(event.target))
+            document.querySelector('body').classList.remove('nav-opened');
+      }
+   }
+
+   elementIsAPanel(target) {
+      var isAPanel = false;
+
+      this.panels.forEach(panel => {
+         if (panel == target)
+            isAPanel = true;
+      });
+
+      return isAPanel;
    }
 }
 
